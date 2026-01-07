13777c9d983e77303675fe22b619f8301e008898
FrancisObinne <obinne.francis@gmail.com>
2026-01-07 03:59:35 -0800

    fixed environment variables

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,16 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const getEnvVar = (key: string, defaultValue?: string): string => {
-    const value = process.env[key];
-    if (!value && !defaultValue) {
-        throw new Error(`Missing required environment variable: ${key}`);
-    }
-    return value || defaultValue!;
-};
-
 export const ENV = {
-    NODE_ENV: process.env.NODE_ENV || "development",
-    PORT: getEnvVar("PORT", "3000"),
-    DB_URL: getEnvVar("DB_URL", "")
-};
+    NODE_ENV : process.env.NODE_ENV || "development",
+    PORT : process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL || ""
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,23 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
 import { ENV } from "./config/env.ts";
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.resolve();
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({ message: "Success!" });
+  res.status(200).json({ message: "Success" });
 });
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../admin/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../admin/dist/index.html"));
-    });
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
-app.listen(ENV.PORT, () => console.log("Server is up and running using typescript! Let's go!!!! Hello E-commerce backend!!!!!! yeeeeeheeee!!!!"));
+
+app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
